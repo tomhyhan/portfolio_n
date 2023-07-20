f32643e2c722ff57e161b6db731111a45c7a82e2
Tom Han <tomhyhan@gmail.com>
2023-07-19 22:44:42 -0700

    algo

--- src/app/algo/page.tsx
+++ src/app/algo/page.tsx
@@ -2,14 +2,16 @@ import Image from 'next/image'
 import { allPosts, Post } from 'contentlayer/generated'
 import { v4 as uuidv4 } from 'uuid';
 import PostCard from '@/components/postCard';
+import { compareDesc } from 'date-fns';
 
 export default function Home() {
-    // console.log(allPosts)
+  const posts = allPosts.sort((a, b) => compareDesc(new Date(a.publishedAt), new Date(b.publishedAt)))
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {allPosts.map((post: Post,) => (
-        <PostCard key={uuidv4()} />
+    <main className="flex min-h-screen items-start 
+                justify-start py-12 px-3 w-full flex-wrap">
+      {posts.map((post: Post,) => (
+        <PostCard key={uuidv4()} post={post} />
       ))}
     </main>
   )
--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import React from 'react'
+import SkillList from './skillList';
 
 export default function Skill({title, skillList}: {
     title: string;
@@ -10,19 +10,7 @@ export default function Skill({title, skillList}: {
           <h2 className="text-white text-xl mb-10">{title}</h2>
           <ol className="flex w-full sm:w-1/2">
             {skillList.map((skill, idx) => (
-              <li key={idx} className="w-20 h-20 min-w-[30] relative mx-5 hover:-translate-y-3">
-                <Image className="w-full h-full border-4 
-                border-gray-200 rounded-xl bg-white object-fill" 
-                  src={`/skills/${skill.image}.png`} 
-                  alt={skill.tag} width={24} height={24} />
-                <div className="flex justify-center items-center absolute 
-                                w-full h-full top-0 left-0 p-2
-                                bg-gray-900 bg-opacity-50 rounded-xl text-center
-                                opacity-0 hover:opacity-80 cursor-pointer">
-                  <span className=" text-white text-sm font-bold
-                  ">{skill.tag}</span>
-                </div>
-              </li>
+              <SkillList key={idx} skill={skill} />
             ))}
           </ol>
         </div>  )
