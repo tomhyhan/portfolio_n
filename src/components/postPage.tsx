a29234d4f5534099ba709c5c807968f665e3140a
tomhyhan <tomhyhan@gmail.com>
2023-07-28 13:23:40 -0700

    creating contain aoc 2020 day7

--- src/components/postPage.tsx
+++ src/components/postPage.tsx
@@ -19,6 +19,7 @@ export default function PostPage({post}: {
     {/* ... thinking about this feature */}
       {/* {error ? <div>loading...</div> : <>{data.view}</>} */}
         <h1 className="text-3xl text-center">{post?.postTitle}</h1>
+            include status
                 <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 mb-16"/>
                 <MdxComponentWraper code={post.body.code} />
     </>
